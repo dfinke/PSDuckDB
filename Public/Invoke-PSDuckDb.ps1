f8ddef643b09652cc718b9a9808588dea09f5ff6
dfinke <finked@hotmail.com>
2024-07-06 11:50:53 -0400

    chore: Refactor Invoke-PSDuckDb.ps1 for better code organization

--- Public/Invoke-PSDuckDb.ps1
+++ Public/Invoke-PSDuckDb.ps1
@@ -1,36 +1,63 @@
 function Invoke-PSDuckDB {
+    <#
+    .EXAMPLE
+CREATE TABLE Cities AS SELECT * FROM 'data/otherData/cities.csv';
+
+PIVOT Cities
+ON Year
+USING sum(Population);
+    #>
     [CmdletBinding()]
     [Alias('psduckdb')]
-    param()
-
-    $ExitOn = @("exit", "quit", "bye")
+    param(        
+        [string]$Command
+    )
 
     $conn = [DuckDB.NET.Data.DuckDBConnection]::new("Data Source=:memory:")
     $conn.Open()
     $duckCommand = $conn.CreateCommand()
-    
-    Write-Host 'Connected to ' -NoNewline
-    Write-Host 'a transient in-memory database' -ForegroundColor Red
 
-    while ($true) {
-        $targetCommand = Read-Host "PSDuckDB"
-        
-        if ($targetCommand -in $ExitOn) {
-            break
-        }
+    if (![string]::IsNullOrEmpty($Command)) {
+        $duckCommand.CommandText = $Command
 
         try {
-            $duckCommand.CommandText = $targetCommand
             $reader = $duckCommand.ExecuteReader()
-            
-            Out-DuckData $reader | Format-Table
+            Out-DuckData $reader
         }
         catch {
             Write-Host $_.Exception.Message -ForegroundColor Red
         }
+        finally {
+            $conn.Close()
+        }
+    }
+    else {
+        $ExitOn = @("exit", "quit", "bye")
+
+        Write-Host "Welcome to PSDuckDB! $(Get-Date)"
+        Write-Host 'Connected to ' -NoNewline
+        Write-Host 'a transient in-memory database' -ForegroundColor Red
+
+        while ($true) {
+            $targetCommand = Read-Host "PSDuckDB"
+        
+            if ($targetCommand -in $ExitOn) {
+                break
+            }
+
+            try {
+                $duckCommand.CommandText = $targetCommand
+                $reader = $duckCommand.ExecuteReader()
+            
+                Out-DuckData $reader | Format-Table
+            }
+            catch {
+                Write-Host $_.Exception.Message -ForegroundColor Red
+            }
+        }
+        $conn.Close()
     }
 
-    $conn.Close()
 }
 
 function Out-DuckData {
