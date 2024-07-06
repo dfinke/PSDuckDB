5c2de9d00c2c1a777aade0f32f0d40b184247461
dfinke <finked@hotmail.com>
2024-07-06 11:16:30 -0400

    feat: Add InstallModule.ps1 script for module installation

---
+++ InstallModule.ps1
@@ -0,0 +1,12 @@
+param ($fullPath)
+#$fullPath = 'C:\Program Files\WindowsPowerShell\Modules\ImportExcel'
+if (-not $fullPath) {
+    $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
+        Where-Object {$_ -notlike ([System.Environment]::GetFolderPath("UserProfile")+"*") -and $_ -notlike "$pshome*"} |
+            Select-Object -First 1
+            $fullPath = Join-Path $fullPath -ChildPath "PSDuckDB"
+}
+
+Push-location $PSScriptRoot
+Robocopy . $fullPath /mir /XD .vscode images .git .github CI __tests__ data mdHelp spikes /XF README.md README.original.md appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
+Pop-Location
