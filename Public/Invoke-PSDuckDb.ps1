ff438bcd10b8a7375f131b301e59e0d062a5e8ac
dfinke <finked@hotmail.com>
2024-07-07 17:30:50 -0400

    chore: Remove unused code and fix formatting in Invoke-PSDuckDb.ps1

--- Public/Invoke-PSDuckDb.ps1
+++ Public/Invoke-PSDuckDb.ps1
@@ -69,19 +69,3 @@ USING sum(Population);
     }
 
 }
-
-function Out-DuckData {
-    param($reader)
-
-    while ($reader.read()) {
-        # Create a hashtable for the current row
-        $rowObject = [Ordered]@{}
-        For ($columnIndex = 0; $columnIndex -lt $reader.FieldCount; $columnIndex++ ) {
-            # Add field name and value as key-value pair
-            $rowObject[$reader.GetName($columnIndex)] = $reader.GetValue($columnIndex)        
-        }
-    
-        # Convert the hashtable to a custom object and add it to the array
-        [PSCustomObject]$rowObject
-    }
-}
