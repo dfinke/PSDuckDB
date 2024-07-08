2bfbde9567764617f991e5b6b3155be2a0ce3dbf
dfinke <finked@hotmail.com>
2024-07-08 07:30:56 -0400

    chore: Refactor New-DuckDBConnection.ps1, add sql and CloseDB methods, and update module dependencies

--- Public/New-DuckDBConnection.ps1
+++ Public/New-DuckDBConnection.ps1
@@ -25,3 +25,67 @@ function New-DuckDBConnection {
   
     [DuckDB.NET.Data.DuckDBConnection]::new("Data Source=$Path")
 }
+
+<#
+.SYNOPSIS
+Executes a SQL query on a DuckDB connection.
+
+.DESCRIPTION
+This script method executes a SQL query on a DuckDB connection. It checks if the connection is open, and if not, it opens the connection. It then creates a command object, sets the command text to the provided query, and executes the command. The result is returned as a DuckDBDataReader object.
+
+.PARAMETER query
+The SQL query to be executed.
+
+.EXAMPLE
+PS> $connection.sql("SELECT * FROM Customers")
+This example executes the SQL query "SELECT * FROM Customers" on the DuckDB connection object named $connection.
+
+#>
+Update-TypeData -TypeName 'DuckDB.NET.Data.DuckDBConnection' -MemberType ScriptMethod -MemberName sql -Force -Value {
+    param([string]$query)
+        
+    if ($this.State -ne 'Open') {
+        $this.Open()
+    }
+        
+    $cmd = $this.CreateCommand()
+    $cmd.CommandText = $query
+
+    $reader = $cmd.ExecuteReader()
+
+    Out-DuckData $reader
+}
+
+<#
+.SYNOPSIS
+Closes the DuckDB connection and disposes of any associated resources.
+
+.DESCRIPTION
+The CloseDB method is used to close the DuckDB connection and dispose of any associated resources such as the reader and command objects.
+
+.PARAMETER None
+
+.INPUTS
+None. You cannot pipe objects to this method.
+
+.OUTPUTS
+None. This method does not generate any output.
+
+.EXAMPLE
+CloseDB
+
+This example demonstrates how to use the CloseDB method to close the DuckDB connection.
+
+#>
+
+Update-TypeData -TypeName 'DuckDB.NET.Data.DuckDBConnection' -MemberType ScriptMethod -MemberName CloseDB -Force -Value {
+    if ($null -ne $reader) {
+        $reader.Dispose()
+    }
+
+    if ($null -ne $cmd) {
+        $cmd.Dispose()
+    }
+
+    $this.Close()
+}
