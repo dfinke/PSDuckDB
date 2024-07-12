652af482f9afecdfb76533714932215fe651df6e
dfinke <finked@hotmail.com>
2024-07-12 16:46:31 -0400

    chore: Update SQL join queries in PowerShell examples

--- examples/08a-join.ps1
+++ examples/08a-join.ps1
@@ -1,11 +1,18 @@
+<# 
+SELECT name, count(*) as number_of_readings
+FROM readings JOIN systems ON id = system_id
+GROUP BY name;
+#>
+
 $db = New-DuckDBConnection
 
 # join customers and orders on customerid
 $sql = @"
-SELECT * 
+SELECT c.Email, count(*) as number_of_orders
 FROM '$PSScriptRoot\..\data\orders\customers.csv' c
 JOIN '$PSScriptRoot\..\data\orders\orders.csv' o
-ON (c.customerid = o.customerid);
+ON (c.customerid = o.customerid)
+GROUP BY c.Email;
 "@
 
 $db.sql($sql)[0]
